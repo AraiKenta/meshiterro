5b10f1d00703284b9f1781a5c991ab87ccd51dc3
AraiKenta <arai.kenta.infratop@gmail.com>
2021-06-30 10:27:02 +0000

    [finish]3章まで

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cddf3f707e6bebb22946e40f73a4c6da97af16b4b534aa6a56b2ac658b637f263d096c107259c8aa28b007a07f0c96cc7dcfc6f614281ee224c76d7d74dea2ba'
